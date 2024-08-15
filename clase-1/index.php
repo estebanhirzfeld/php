abff3e8c4251c3bff0b5e81af9c4696250270fec
Steve <estebanhirzfeld@gmail.com>
2024-08-15 17:35:34 -0300

    clase 1: html + php

--- clase-1/index.php
+++ clase-1/index.php
@@ -1,3 +1,14 @@
 <?php
-echo "Hello World"
+    $var = "World"
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style="text-align:center">Hello <?php echo $var ?></h1>
+</body>
+</html>
